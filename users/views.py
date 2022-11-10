2e37c628bbb12c9be899f8d1f0280aaeaa5ea2e1
WANGSHAN <letsplayshan@hotmail.com>
2022-11-10 09:11:54 +0800

    kakao login

--- users/views.py
+++ users/views.py
@@ -139,7 +139,7 @@ class GithubLogIn(APIView):
         try:
             code = request.data.get("code")
             access_token = requests.post(
-                #원본 f"https://github.com/login/oauth/access_token?code={code}&client_id=5195598d392601f20eea&client_secret={settings.GH_SECRET}",
+                
                 f"https://github.com/login/oauth/access_token?code={code}&client_id=5f121645d89d0532ca49&client_secret={settings.GH_SECRET}",
                 headers={"Accept": "application/json"},
             )
